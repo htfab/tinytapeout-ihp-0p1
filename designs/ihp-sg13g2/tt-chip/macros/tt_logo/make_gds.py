8df4fb06bef91673261c08dc1ac21a0ca773ce9c
htfab <mpw@htamas.net>
2024-07-31 14:53:08 +0200

    feat: replace the logo slit pattern

--- designs/ihp-sg13g2/tt-chip/macros/tt_logo/make_gds.py
+++ designs/ihp-sg13g2/tt-chip/macros/tt_logo/make_gds.py
@@ -13,41 +13,24 @@ DATATYPE_DRAWING = 0
 DATATYPE_SLIT = 24
 PIXEL_SIZE = 2.0 # um
 
-PATTERN = (2, 2), (4, 2), (0, 4)
-
-# Convert the image to grayscale
-img = img.convert("L")
-
-slits = set()
-for j in range(img.height):
-    for i in range(img.width):
-        slits.add((i, j))
+# Convert the image to RGB
+img = img.convert("RGB")
 
 layout = gdspy.Cell("tt_logo")
 for y in range(img.height):
     for x in range(img.width):
-        color = img.getpixel((x, y))
+        r, g, b = img.getpixel((x, y))
         # Adjust y-coordinate to flip the image vertically
         flipped_y = img.height - y - 1
-        if color < 128:
+        if g < 128:
             layout.add(
                 gdspy.Rectangle((x * PIXEL_SIZE, flipped_y * PIXEL_SIZE),
                                 ((x + 1) * PIXEL_SIZE, (flipped_y + 1) * PIXEL_SIZE),
                                 layer=LAYER, datatype=DATATYPE_DRAWING))
-        else:
-            for j in range(PATTERN[0][1]+2):
-                for i in range(PATTERN[0][0]+2):
-                    slits.discard((x-i, flipped_y-j))
-
-k = img.height + img.width
-for j in range(-k, k+1):
-    for i in range(-k, k+1):
-        x = PATTERN[1][0] * i + PATTERN[2][0] * j
-        y = PATTERN[1][1] * i + PATTERN[2][1] * j
-        if (x, y) in slits:
+        if r > 128 and g < 128:
             layout.add(
-                gdspy.Rectangle(((x + 1) * PIXEL_SIZE, (y + 1) * PIXEL_SIZE),
-                                ((x + 1 + PATTERN[0][0]) * PIXEL_SIZE, (y + 1 + PATTERN[0][1]) * PIXEL_SIZE),
+                gdspy.Rectangle((x * PIXEL_SIZE, flipped_y * PIXEL_SIZE),
+                                ((x + 1) * PIXEL_SIZE, (flipped_y + 1) * PIXEL_SIZE),
                                 layer=LAYER, datatype=DATATYPE_SLIT))
 
 # Save the layout to a file
